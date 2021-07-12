93e4d2d19396c89b0dcc3b2cc0b2593290c2f561
Ben Shekhtman <benshekhtman@hotmail.com>
2021-07-12 14:57:33 -0400

    staged for prod

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-const url = 'http://localhost:3001/todos'
+//DEV URL
+// const url = 'http://localhost:3001/todos'
+
+//PROD URL
+const url = 'https://mern-todos-ben.herokuapp.com/todos'
 
 export const readTodos = () => axios.get(url)
 export const createTodo = (newTodo) => axios.post(url, newTodo)
